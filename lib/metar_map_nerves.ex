ad54f1491ecbd0dbe9fa3a0c931563ba58328f99
Rockwell Schrock <rockwell@schrock.me>
2022-02-18 16:21:44 -0500

    `mix nerves.new metar_map_nerves`

---
+++ lib/metar_map_nerves.ex
@@ -0,0 +1,18 @@
+defmodule MetarMapNerves do
+  @moduledoc """
+  Documentation for MetarMapNerves.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MetarMapNerves.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/metar_map_nerves_test.exs
@@ -0,0 +1,8 @@
+defmodule MetarMapNervesTest do
+  use ExUnit.Case
+  doctest MetarMapNerves
+
+  test "greets the world" do
+    assert MetarMapNerves.hello() == :world
+  end
+end
