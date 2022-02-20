4931fdee82d2207c15c985c4e0c103a6905064de
Rockwell Schrock <rockwell@schrock.me>
2022-02-20 15:12:37 -0500

    More LV stuff

--- lib/metar_map_web.ex
+++ lib/metar_map_web.ex
@@ -27,6 +27,23 @@ defmodule MetarMapWeb do
     end
   end
 
+  def view_helpers do
+    quote do
+      # Use all HTML functionality (forms, tags, etc)
+      use Phoenix.HTML
+
+      # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
+      import Phoenix.LiveView.Helpers
+
+      # Import basic rendering functionality (render, render_layout, etc)
+      import Phoenix.View
+
+      import MetarMapWeb.ErrorHelpers
+      import MetarMapWeb.Gettext
+      alias MetarMapWeb.Router.Helpers, as: Routes
+    end
+  end
+
   def view do
     quote do
       use Phoenix.View,
@@ -36,14 +53,31 @@ defmodule MetarMapWeb do
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
 
-      # Use all HTML functionality (forms, tags, etc)
-      use Phoenix.HTML
+      unquote(view_helpers())
+    end
+  end
 
-      import Phoenix.LiveView.Helpers
+  def live_view do
+    quote do
+      use Phoenix.LiveView, layout: {MetarMapWeb.LayoutView, "live.html"}
 
-      import MetarMapWeb.ErrorHelpers
-      import MetarMapWeb.Gettext
-      alias MetarMapWeb.Router.Helpers, as: Routes
+      unquote(view_helpers())
+    end
+  end
+
+  def live_component do
+    quote do
+      use Phoenix.LiveComponent
+
+      unquote(view_helpers())
+    end
+  end
+
+  def component do
+    quote do
+      use Phoenix.Component
+
+      unquote(view_helpers())
     end
   end
 
--- lib/metar_map_web/router.ex
+++ lib/metar_map_web/router.ex
@@ -5,6 +5,7 @@ defmodule MetarMapWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
+    plug :put_root_layout, {MetarMapWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
