a0eba88253c4a0f80faa25b2f4e3e5d1a5f95b0e
Rockwell Schrock <rockwell@schrock.me>
2022-08-27 22:43:47 -0400

    Add HOSTNAME config; bump nerves versions

--- config/target.exs
+++ config/target.exs
@@ -87,7 +87,7 @@ config :mdns_lite,
   # "nerves.local" for convenience. If more than one Nerves device is on the
   # network, delete "nerves" from the list.
 
-  host: [:hostname, "metar-map"],
+  host: [:hostname, System.get_env("HOSTNAME", "metar-map")],
   ttl: 120,
 
   # Advertise the following services over mDNS.
--- mix.exs
+++ mix.exs
@@ -68,15 +68,15 @@ defmodule MetarMap.MixProject do
       # bumps to Nerves systems. Since these include Linux kernel and Erlang
       # version updates, please review their release notes in case
       # changes to your application are needed.
-      {:nerves_system_rpi, "~> 1.17", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.17", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.17", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.17", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.17", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.17", runtime: false, targets: :rpi4},
-      {:nerves_system_bbb, "~> 2.12", runtime: false, targets: :bbb},
-      {:nerves_system_osd32mp1, "~> 0.8", runtime: false, targets: :osd32mp1},
-      {:nerves_system_x86_64, "~> 1.17", runtime: false, targets: :x86_64}
+      # {:nerves_system_rpi, "1.20.0", runtime: false, targets: :rpi},
+      {:nerves_system_rpi0, "~> 1.20", runtime: false, targets: :rpi0},
+      # {:nerves_system_rpi2, "1.20.0", runtime: false, targets: :rpi2},
+      # {:nerves_system_rpi3, "1.20.0", runtime: false, targets: :rpi3},
+      {:nerves_system_rpi3a, "~> 1.20", runtime: false, targets: :rpi3a}
+      # {:nerves_system_rpi4, "1.20.0", runtime: false, targets: :rpi4},
+      # {:nerves_system_bbb, "~> 2.12", runtime: false, targets: :bbb},
+      # {:nerves_system_osd32mp1, "~> 0.8", runtime: false, targets: :osd32mp1},
+      # {:nerves_system_x86_64, "1.20.0", runtime: false, targets: :x86_64}
     ]
   end
 
@@ -94,7 +94,7 @@ defmodule MetarMap.MixProject do
 
   defp aliases do
     [
-      "assets.deploy": ["esbuild default --minify", "phx.digest"],
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"],
       firmware: ["assets.deploy", "firmware"]
     ]
   end
